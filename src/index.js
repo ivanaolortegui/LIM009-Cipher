7ba2ce30353cc4599041de6cd87a616818330ed2
ivanaolortegui <olorteguimor@gmail.com>
2019-02-22 10:26:43 -0500

    Agragando maquetado básico a mi html  y probando con la función encode para cifrar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 /* Acá va tu código */
+const inputName = document.getElementById('input-name');
+const inputSurname = document.getElementById('input-surname');
+const offset = document.getElementById('offset');
+const outputName = document.getElementById('output-name');
+const btnEncode = document.getElementById('btn-encode');
+
+
+const encode = (inputN, inputS, offset) => {
+  
+  let output = '';
+  for (let i = 0; i < inputN.length; i++) {
+    const codeUni = inputN.charCodeAt(i);
+    const formula = String.fromCharCode((codeUni - 65 + offset) % 26 + 65);
+    output += formula;
+  }
+  outputName.innerHTML = output /// para pintar lo que me retorna la función 
+};
+
+btnEncode.addEventListener('click', () => {
+  const input1 = inputName.value;
+  const offset1 = offset.value;
+  encode(input1, inputSurname, offset1)
+})
