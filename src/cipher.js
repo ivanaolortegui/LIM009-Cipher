5e8afc2391a1f21de434f4c4c473e15757b03def
ivanaolortegui <olorteguimor@gmail.com>
2019-02-23 03:42:59 -0500

    Agragando imagen de fondoy función decode para descifrar

--- src/cipher.js
+++ src/cipher.js
@@ -3,13 +3,28 @@ window.cipher = {
     let output = '';
     for (let i = 0; i < inputName.length; i++) {
       const codeUni = inputName.charCodeAt(i);
-      const formula = String.fromCharCode((codeUni - 65 + offset) % 26 + 65);
-      output += formula;
-  }
-  return output
-    /* Acá va tu código */
+      if(codeUni>=65 && codeUni<=90) {
+        const formula = String.fromCharCode((codeUni - 65 + offset) % 26 + 65);
+        output += formula;
+      } else if(codeUni>=97&&codeUni<=122){
+        const formula = String.fromCharCode((codeUni - 97 + offset) % 26 + 97);
+        output += formula;
+      } else if(codeUni >= 33 && codeUni<= 64) {
+        const formula = String.fromCharCode((codeUni-33 + offset)%31 + 33);
+        output +=formula;
+      } else {
+        output +=String.fromCharCode(codeUni);
+      }
+    }
+    return output;
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (inputName,inputSurname,offset) => {
+    let output= '';
+    for (let i = 0; i < inputName.length; i++) {
+    const codeUni = inputName.charCodeAt(i);
+      const formula = String.fromCharCode((codeUni - 65 - offset) % 26 + 65);
+      output += formula; 
+    }
+    return output;
   }
 };
--- src/index.js
+++ src/index.js
@@ -3,16 +3,35 @@ const inputName = document.getElementById('input-name');
 const inputSurname = document.getElementById('input-surname');
 const offset = document.getElementById('offset');
 const outputName = document.getElementById('output-name');
-const btnEncode = document.getElementById('btn-encode');
 
+const btnEncode = document.getElementById('btn-encode');
+const inputNameEncode = document.getElementById('input-name-encode');
+const inputSurnameEncode = document.getElementById('input-surname-encode');
+const offsetEncode = document.getElementById('offset-encode');
+const outputNameDecode = document.getElementById('output-name-decode');
+const btnDecode = document.getElementById('btn-decode');
 
 const functionEncode = (inputN, inputS, offset) => {
   let output = window.cipher.encode(inputN,inputS,offset);
   outputName.innerHTML = output /// para pintar lo que me retorna la función 
 };
+const functionDecode = (inputN, inputS, offset) => {
+  let output = window.cipher.decode(inputN,inputS,offset);
+  outputNameDecode.innerHTML = output
+};
 
-btnEncode.addEventListener('click', () => {
+btnDecode.addEventListener('click', () => {
+  let input1 = inputNameEncode.value;
+  let offset1 = offsetEncode.value;
+  functionDecode(input1, inputSurnameEncode, offset1)
+})
+
+ btnEncode.addEventListener('click', () => {
   let input1 = inputName.value;
   let offset1 = offset.value;
   functionEncode(input1, inputSurname, offset1)
-})
+}) 
+
+
+
+
