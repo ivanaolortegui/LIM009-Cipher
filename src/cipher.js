0688f5029cb53eda7fe08f8321981b6bb6a3611e
ivanaolortegui <olorteguimor@gmail.com>
2019-02-24 21:55:34 -0500

    Agragando el logo de la página y editando la interfaz de usuario para descifrar nombres

--- src/cipher.js
+++ src/cipher.js
@@ -1,28 +1,48 @@
 window.cipher = {
   encode: (inputName,inputSurname,offset) => {
     let output = '';
+    let output2 = '';
     for (let i = 0; i < inputName.length; i++) {
-      const codeUni = inputName.charCodeAt(i);
-      if(codeUni>=65 && codeUni<=90) {
-        const formula = String.fromCharCode((codeUni - 65 + offset) % 26 + 65);
+      const codeAscci = inputName.charCodeAt(i);
+      if(codeAscci >=65 && codeAscci<=90) {
+        const formula = String.fromCharCode((codeAscci - 65 + offset) % 26 + 65);
         output += formula;
-      } else if(codeUni>=97&&codeUni<=122){
-        const formula = String.fromCharCode((codeUni - 97 + offset) % 26 + 97);
+      } else if(codeAscci>=97&&codeAscci<=122){
+        const formula = String.fromCharCode((codeAscci - 97 + offset) % 26 + 97);
         output += formula;
-      } else if(codeUni >= 33 && codeUni<= 64) {
-        const formula = String.fromCharCode((codeUni-33 + offset)%31 + 33);
+      } else if(codeAscci >= 33 && codeAscci<= 64) {
+        const formula = String.fromCharCode((codeAscci-33 + offset)%31 + 33);
         output +=formula;
       } else {
-        output +=String.fromCharCode(codeUni);
+        output +=String.fromCharCode(codeAscci);
+      }
+     
+    }
+     return output;
+    for (let i = 0; i < inputSurname.length; i++) {
+      const codeAscci = inputSurname.charCodeAt(i);
+      if(codeAscci>=65 && codeAscci<=90) {
+        const formula = String.fromCharCode((codeAscci - 65 + offset) % 26 + 65);
+        output2 += formula;
+      } else if(codeAscci>=97&&codeAscci<=122){
+        const formula = String.fromCharCode((codeAscci - 97 + offset) % 26 + 97);
+        output2 += formula;
+      } else if(codeAscci >= 33 && codeAscci<= 64) {
+        const formula = String.fromCharCode((codeAscci-33 + offset)%31 + 33);
+        output2 +=formula;
+      } else {
+        output2 +=String.fromCharCode(codeAscci);
       }
     }
-    return output;
+    return output2;
+
+
   },
   decode: (inputName,inputSurname,offset) => {
     let output= '';
     for (let i = 0; i < inputName.length; i++) {
-    const codeUni = inputName.charCodeAt(i);
-      const formula = String.fromCharCode((codeUni - 65 - offset) % 26 + 65);
+    const codeAscci = inputName.charCodeAt(i);
+      const formula = String.fromCharCode((codeAscci - 65 - offset %26) % 26 + 65);
       output += formula; 
     }
     return output;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,53 @@
 /* Acá va tu código */
+
+// Los ids de los formularios se guardan en constantes para luego ser manipulados. 
 const inputName = document.getElementById('input-name');
 const inputSurname = document.getElementById('input-surname');
 const offset = document.getElementById('offset');
 const outputName = document.getElementById('output-name');
-
+const outputSuername = document.getElementById('output-surname');
 const btnEncode = document.getElementById('btn-encode');
 const inputNameEncode = document.getElementById('input-name-encode');
 const inputSurnameEncode = document.getElementById('input-surname-encode');
 const offsetEncode = document.getElementById('offset-encode');
 const outputNameDecode = document.getElementById('output-name-decode');
 const btnDecode = document.getElementById('btn-decode');
+/* const inputNumberEncode = document.getElementById('input-number-encode');
+const inputCodeEncode = document.getElementById('input-code-encode');
+const offsetCarEncode = document.getElementById('offset-card-encode'); */
 
+if(btnEncode){
 const functionEncode = (inputN, inputS, offset) => {
   let output = window.cipher.encode(inputN,inputS,offset);
-  outputName.innerHTML = output /// para pintar lo que me retorna la función 
+  let output2 = window.cipher.encode(inputS,inputS, offset);
+  outputName.innerHTML = output 
+  outputSuername.innerHTML = output2 /* Aqui estoy pintando el valor que me 
+  retorna la función del respectivo objeto con su propiedad que estoy llamando. */
+  
 };
+
+  btnEncode.addEventListener('click', () => {
+    let inputNameValue = inputName.value;
+    let offsetValue = offset.value;
+    let inputSurnameValue = inputSurname.value;
+    functionEncode(inputNameValue, inputSurnameValue, offsetValue)
+  }) 
+}else{
 const functionDecode = (inputN, inputS, offset) => {
   let output = window.cipher.decode(inputN,inputS,offset);
   outputNameDecode.innerHTML = output
 };
+ 
+  btnDecode.addEventListener('click', () => {
+    let inputNameValue = inputNameEncode.value;
+    let inputSurnameValue = inputSurnameEncode.value
+    let offsetValue = offsetEncode.value;
+    functionDecode(inputNameValue, inputSurnameValue, offsetValue)
+  })
+}
 
-btnDecode.addEventListener('click', () => {
-  let input1 = inputNameEncode.value;
-  let offset1 = offsetEncode.value;
-  functionDecode(input1, inputSurnameEncode, offset1)
-})
 
- btnEncode.addEventListener('click', () => {
-  let input1 = inputName.value;
-  let offset1 = offset.value;
-  functionEncode(input1, inputSurname, offset1)
-}) 
-
+ 
 
 
 
